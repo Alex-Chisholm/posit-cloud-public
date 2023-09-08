f4ee8d71c5e87d177cbad372bef4aeab57d366f5
Alex-Chisholm <ajc2357@yahoo.com>
2023-09-08 17:16:38 +0000

    exclaimation!!!

--- git.R
+++ git.R
@@ -6,4 +6,7 @@ usethis::gh_token_help()
 usethis::git_sitrep()
 
 
-usethis::edit_r_environ()
+# in terminal
+
+# git config --global user.email "ajc2357@yahoo.com"
+# git config --global user.name "Alex-Chisholm"
