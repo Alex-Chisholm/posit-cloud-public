a4c8ce3b0c8d62e36f5a3170c33802e9680afdb8
Alex-Chisholm <ajc2357@yahoo.com>
2023-09-08 21:38:21 +0000

    asdf

--- git.R
+++ git.R
@@ -9,7 +9,7 @@ usethis::git_sitrep()
 
 # in terminal - this is great?
 
-git config --global credential.helper 'cache --timeout 3600'
+git config --global credential.helper 'cache --timeout 604800'
 git config --global user.email "ajc2357@yahoo.com"
 git config --global user.name "Alex-Chisholm"
 git config --global --list
